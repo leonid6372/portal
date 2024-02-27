c3ab1322619bd35d86772897aa2b6d77fd83f877
2po4ill <90243310+2po4ill@users.noreply.github.com>
2024-02-27 07:45:35 +0300

    Update config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -28,7 +28,7 @@ type HTTPServer struct {
 
 func MustLoad() *Config {
 	//configPath := os.Getenv("CONFIG_PATH") Вариант для прода через системный путь
-	configPath := "C:/Users/Leonid/Desktop/portal/config/local.yaml" // Локальный путь
+	configPath := "portal/config/local.yaml" // Локальный путь
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
