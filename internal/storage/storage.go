5dd8af070eb683e754d86e85066a9ab3fac49c9f
leonid6372 <lv.solovyev@yandex.ru>
2024-03-19 14:01:11 +0300

    auth and ORM

--- internal/storage/storage.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-import "errors"
-
-var (
-	ErrItemUnavailable = errors.New("Item with selected item_id is not available for order")
-	ErrPassword        = errors.New("Password do not match")
-)
